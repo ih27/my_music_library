31c2b2373f0178f7d8bc11a78fef15ee98c8573a
Ismayil Hasanov <ismailhasanov@gmail.com>
2024-06-14 13:26:39 +0300

    fix: sorting annoyances

--- app/controllers/tracks_controller.rb
+++ app/controllers/tracks_controller.rb
@@ -1,20 +1,30 @@
 class TracksController < ApplicationController
   def index
-    @tracks = Track.all.includes(:artists, :key, :playlists)
+    tracks = Track.all.includes(:artists, :key, :playlists)
 
     if params[:sort].present? && params[:direction].present?
       column = params[:sort]
       direction = params[:direction]
 
-      if column == 'keys.name'
-        @tracks = @tracks.sort_by { |track| natural_sort_key(track.key&.name) }
-        @tracks.reverse! if direction == 'desc'
+      case column
+      when 'keys.name'
+        tracks = tracks.sort_by { |track| natural_sort_key(track.key&.name) }
+        tracks.reverse! if direction == 'desc'
+      when 'artists.name'
+        tracks = tracks.sort_by { |track| track.artists.map(&:name).join(", ") }
+        tracks.reverse! if direction == 'desc'
+      when 'playlists.name'
+        tracks = tracks.sort_by { |track| natural_sort_key(track.playlists.map(&:name).join(", ")) }
+        tracks.reverse! if direction == 'desc'
       else
-        @tracks = @tracks.order("#{column} #{direction}")
+        tracks = tracks.order("#{column} #{direction}")
       end
+      # Use pagy_array for array results
+      @pagy, @tracks = pagy_array(tracks)
+    else
+      # Use pagy for ActiveRecord::Relation
+      @pagy, @tracks = pagy(tracks)
     end
-
-    @pagy, @tracks = pagy(@tracks) # Use pagy_array for array pagination
   end
 
   private
--- config/initializers/pagy.rb
+++ config/initializers/pagy.rb
@@ -28,7 +28,7 @@ Pagy::DEFAULT[:size]        = [1,2,2,1]
 
 # Array extra: Paginate arrays efficiently, avoiding expensive array-wrapping and without overriding
 # See https://ddnexus.github.io/pagy/docs/extras/array
-# require 'pagy/extras/array'
+require 'pagy/extras/array'
 
 # Calendar extra: Add pagination filtering by calendar time unit (year, quarter, month, week, day)
 # See https://ddnexus.github.io/pagy/docs/extras/calendar
